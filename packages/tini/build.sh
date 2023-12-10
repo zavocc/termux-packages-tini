542bebf57be5ebdf87f319cbe9a7ad2ecfad196c
zavocc <zavocc1030@outlook.com>
2023-12-10 17:59:17 +0800

    new package: tini

---
+++ packages/tini/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/krallin/tini/"
+TERMUX_PKG_DESCRIPTION="A tiny but valid init for containers"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="Wyatt Marcus (zavocc1030@outlook.com)"
+TERMUX_PKG_VERSION=0.19.0
+TERMUX_PKG_SRCURL="https://github.com/krallin/tini/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=0fd35a7030052acd9f58948d1d900fe1e432ee37103c5561554408bdac6bbf0d
