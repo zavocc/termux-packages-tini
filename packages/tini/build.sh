dd9d3b12f2fbfa4ceca35ef569fe91bb0b033c64
zavocc <zavocc1030@outlook.com>
2023-12-12 16:52:18 +0800

    tini: fix incorrect variable name in build.sh

--- packages/tini/build.sh
+++ packages/tini/build.sh
@@ -4,5 +4,5 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Wyatt Marcus (zavocc1030@outlook.com)"
 __COMMIT=0b44d3665869e46ccbac7414241b8256d6234dc4
 TERMUX_PKG_VERSION=0.19.0
-TERMUX_PKG_SRCURL="https://github.com/krallin/tini/archive/${COMMIT}.tar.gz"
+TERMUX_PKG_SRCURL="https://github.com/krallin/tini/archive/${__COMMIT}.tar.gz"
 TERMUX_PKG_SHA256=3c17473bc78260a88ff8dc5e02034c96a8874f68b57ae9c59b42859721ffa08c
